21406b07cf5c19d0b91a3a48f413a35c1059066c
asilajdzic <asilajdzic2000@gmail.com>
2023-04-02 23:39:40 +0200

    added map and article preview component

--- src/components/article-preview/article-preview.component.jsx
+++ src/components/article-preview/article-preview.component.jsx
@@ -2,7 +2,7 @@ import './article-preview.styles.scss';
 
 const ArticlePreview = ({ article }) => {
 	const { title, text, imageUrl } = article;
-
+	console.log(imageUrl);
 	return (
 		<div className='article-container'>
 			<h1>{title}</h1>
