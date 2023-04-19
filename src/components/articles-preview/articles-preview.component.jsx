bf2bd10a77d0ed6eab0073756b4dc8176343e7eb
asilajdzic <asilajdzic2000@gmail.com>
2023-04-19 03:10:07 +0200

    added spinner component

--- src/components/articles-preview/articles-preview.component.jsx
+++ src/components/articles-preview/articles-preview.component.jsx
@@ -1,14 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 
 import { getCollection } from '../../utils/firebase/firebase.utils';
 
 import ArticlePreview from '../article-preview/article-preview.component';
+import Spinner from '../spinner/spinner.component';
 
 import './articles-preview.styles.scss';
 
 const ArticlesPreview = () => {
 	const [articles, setArticles] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 	useEffect(() => {
+		setIsLoading(true);
 		getCollection('Articles').then((data) => {
 			const articles = data;
 			const sortedArticles = articles.sort(
@@ -16,17 +19,24 @@ const ArticlesPreview = () => {
 			);
 			setArticles(sortedArticles);
 		});
+		setIsLoading(false);
 	}, []);
 	return (
-		<div className='articles-container'>
-			<h1>Latest news</h1>
-			{articles.map(
-				(article, index) =>
-					index < 7 && (
-						<ArticlePreview article={article} key={index} index={index} />
-					)
+		<Fragment>
+			{isLoading ? (
+				<Spinner />
+			) : (
+				<div className='articles-container'>
+					<h1>Latest news</h1>
+					{articles.map(
+						(article, index) =>
+							index < 7 && (
+								<ArticlePreview article={article} key={index} index={index} />
+							)
+					)}
+				</div>
 			)}
-		</div>
+		</Fragment>
 	);
 };
 
--- src/components/department/department.component.jsx
+++ src/components/department/department.component.jsx
@@ -3,22 +3,31 @@ import { useState, useEffect } from 'react';
 import { getCollection } from '../../utils/firebase/firebase.utils';
 
 import InfoCard from '../info-card/info-card.component';
+import Spinner from '../spinner/spinner.component';
 
 import './department.styles.scss';
 
 const Department = ({ department }) => {
 	const [departmentArray, setDepartmentArray] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
 	useEffect(() => {
 		const getDepartmentEmployees = async () => await getCollection(department);
+		setIsLoading(true);
 		getDepartmentEmployees().then((data) => setDepartmentArray(data));
+		setIsLoading(false);
 		// eslint-disable-next-line
 	}, []);
 	return (
 		<div className='department-container'>
 			<h1>{department}</h1>
-			{departmentArray.map((employee, index) => (
-				<InfoCard employee={employee} key={index} />
-			))}
+			{isLoading ? (
+				<Spinner />
+			) : (
+				departmentArray.map((employee, index) => (
+					<InfoCard employee={employee} key={index} />
+				))
+			)}
 		</div>
 	);
 };
